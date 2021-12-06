3246c5e81c694f615589f42f0f8cd4a50ec29b61
lethiferal <95504439+lethiferal@users.noreply.github.com>
2021-12-07 01:25:54 +0530

    Update weather.js

--- Commands/C1/weather.js
+++ Commands/C1/weather.js
@@ -4,14 +4,14 @@ module.exports = {
    aliases: ["wr","temperature","forecast"],
    type:"default",
    code:`$title[Weather report for $message]
-$description[\`\`\`Location: $jsonRequest[http://api.somecool.repl.co/weather?place=$message[1]+$message[2]+$message[3]+$message[4]+$message[5];location;No data.]\`\`\`
-\`\`\`Temperature: $jsonRequest[http://api.somecool.repl.co/weather?place=$message[1]+$message[2]+$message[3]+$message[4]+$message[5];temperature;No data.]\`\`\`
-\`\`\`Humidity: $jsonRequest[http://api.somecool.repl.co/weather?place=$message[1]+$message[2]+$message[3]+$message[4]+$message[5];humidity;No data.]\`\`\`
-\`\`\`Wind Speed: $jsonRequest[http://api.somecool.repl.co/weather?place=$message[1]+$message[2]+$message[3]+$message[4]+$message[5];wind_display;No data.]\`\`\`
-\`\`\`Observation Time: $jsonRequest[http://api.somecool.repl.co/weather?place=$message[1]+$message[2]+$message[3]+$message[4]+$message[5];observation_time;No data.]\`\`\`]
+$description[\`\`\`Location: $jsonRequest[http://api.somecool.repl.co/weather?place=$message;location;No data.]\`\`\`
+\`\`\`Temperature: $jsonRequest[http://api.somecool.repl.co/weather?place=$message;temperature;No data.]\`\`\`
+\`\`\`Humidity: $jsonRequest[http://api.somecool.repl.co/weather?place=$message;humidity;No data.]\`\`\`
+\`\`\`Wind Speed: $jsonRequest[http://api.somecool.repl.co/weather?place=$message;wind_display;No data.]\`\`\`
+\`\`\`Observation Time: $jsonRequest[http://api.somecool.repl.co/weather?place=$message;observation_time;No data.]\`\`\`]
 $color[000001]
-$thumbnail[$jsonRequest[http://api.somecool.repl.co/weather?place=$message[1]+$message[2]+$message[3]+$message[4]+$message[5];image_url; ]]
-$image[https://wttr.in/$message[1]+$message[2]+$message[3]+$message[4]+$message[5].png?m]
-$onlyIf[$message[1]!=;> **:information_source: Add a place.**]
-$onlyIf[$jsonRequest[http://api.somecool.repl.co/weather?place=$message[1]+$message[2]+$message[3]+$message[4]+$message[5];location;]!=;> **:information_source: No place found with the name "$message[1]+$message[2]+$message[3]+$message[4]+$message[5]".**]`
+$thumbnail[$jsonRequest[http://api.somecool.repl.co/weather?place=$message;image_url; ]]
+$image[https://wttr.in/$message.png?m]
+$onlyIf[$message!=;> **:information_source: Add a place.**]
+$onlyIf[$jsonRequest[http://api.somecool.repl.co/weather?place=$message;location;]!=;> **:information_source: No place found with the name "$message".**]`
    }
