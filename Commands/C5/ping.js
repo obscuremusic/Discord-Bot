31d42ceea5249169ab70c41c69c96aad95473910
lethiferal <95504439+lethiferal@users.noreply.github.com>
2021-12-07 01:45:48 +0530

    Update and rename help.js to ping.js

---
+++ Commands/C5/ping.js
@@ -0,0 +1,7 @@
+module.exports ={
+  name:"ping",
+  aliases:["latency","pong","delay"],
+  code:`> :information_source: **Collecting Ping...**
+$editIn[1s;> :ping_pong: **Pong!** \`$ping ms\`]
+$deleteIn[5s]`
+}
