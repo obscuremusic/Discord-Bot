51de979c357aa2f90d56c159de2c92567ca260aa
lethiferal <95504439+lethiferal@users.noreply.github.com>
2021-12-14 01:57:22 +0530

    Update cleverbot.js

--- Commands/C5/cleverbot.js
+++ Commands/C5/cleverbot.js
@@ -1,6 +1,6 @@
 module.exports ={
 name:"$alwaysExecute",
-code:`$jsonRequest[https://api.affiliateplus.xyz/api/chatbot?message=$message&botname=UMC%20ChatBot&ownername=Lethiferal&user=$authorID;message;> :information_source: **An error occured**]
+code:`$replaceText[$jsonRequest[https://api.affiliateplus.xyz/api/chatbot?message=$message&botname=UMC%20ChatBot&ownername=Lethiferal&user=$authorID;message;> :information_source: **An error occured**];$authorID;$username[$authorID]]
 $reply[$messageID;no]
 $botTyping
 $onlyIf[$channelID==$getServerVar[chatbotchannel];]`
