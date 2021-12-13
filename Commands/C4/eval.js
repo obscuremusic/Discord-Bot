5c2edc03c2b883ca6163243a01221ca20a855c2d
lethiferal <95504439+lethiferal@users.noreply.github.com>
2021-12-14 01:31:12 +0530

    Create eval.js

---
+++ Commands/C4/eval.js
@@ -0,0 +1,6 @@
+module.exports ={
+  name:"eval",
+  aliases:["evaluate","run"],
+  code:`$eval[$message]
+  $onlyForIDs[888807741571858442;589866849815232573;> :information_source: **It's only for the botmaster, stupid.**]`
+}
