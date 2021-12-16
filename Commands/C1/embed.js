a61f9a4f06446b9d52789ef6582209ae8aa49cf3
lethiferal <95504439+lethiferal@users.noreply.github.com>
2021-12-16 20:40:49 +0530

    Update embed.js

--- Commands/C1/embed.js
+++ Commands/C1/embed.js
@@ -4,5 +4,5 @@ aliases:["json"],
 code:`$djsEval[message.channel.send($getObject)]
 $createObject[$message]
 $onlyIf[$isValidObject[$message]==true;> :information_source: **Not a valid json, use this website to get embed json - https://embedbuilder.nadekobot.me/** ]
-$onlyForIDs[589866849815232573; > :information_source: **You are not the bot owner!**]`
+$onlyForIDs[589866849815232573;888807741571858442; > :information_source: **You are not the bot owner!**]`
 }
