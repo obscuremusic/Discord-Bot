4fcc1afd5ec9a45c13e9da4b7c1c69e809592376
lethiferal <95504439+lethiferal@users.noreply.github.com>
2021-12-18 00:37:06 +0530

    Update tips.js

--- Commands/C3/tips.js
+++ Commands/C3/tips.js
@@ -25,16 +25,16 @@ name:"guitarchord",
 type:"awaited",
 code:`$author[1;Music Theory Advice;https://cdn.discordapp.com/attachments/889976693102628904/921376597313978378/20211217_175129.png]
 $title[1;Interpreting Guitar Chord Charts] 
-$description[1;One of the first skills a guitarist should learn is how to read guitar chord charts. However, newbies may mistake the lines, numbers, and symbols for math rather than music.
-Three or more separate notes are played together to form a chord. An interval, often known as a double-stop, is two separate notes played together.
-Your guitar strings are represented by the vertical lines on the guitar chart. Because the chart is a vertical display and you're gazing down at your instrument horizontally, it might be difficult for novices to get the feel of it.
-The horizontal lines on your guitar's neck symbolise the fret bars.
+$description[1;Three or more separate notes are played together to form a chord. An interval, often known as a double-stop, is two separate notes played together.
+
+- Your guitar strings are represented by the vertical lines on the guitar chart. Because the chart is a vertical display and you're gazing down at your instrument horizontally, it might be difficult for novices to get the feel of it.
+
+- The horizontal lines on your guitar's neck symbolise the fret bars.
 The dots indicate where your fingers should be placed on the fretboard. They're positioned on a vertical line to indicate the correct string, and inside the fret boxes to indicate which fret to press for that string. If there isn't a dot on the fretboard, that signifies you shouldn't touch that string.
-Sometimes there will be numbers in the dots, or numbers will be seen beneath the chart.
-Numbers can be found at the bottom of the chart or among the dots. Each of your fingers is represented by a number. A number next to a vertical line indicates which finger should be used on that string.
-X means to avoid strumming the string at all or mute it.
-O means to strum the string open, which means don’t press it onto the fretboard. 
-And lastly, the letters at the top of the chord chart are the name of the chord.]
+
+- Sometimes there will be numbers in the dots, or numbers will be seen beneath the chart.Each of your fingers is represented by a number. A number next to a vertical line indicates which finger should be used on that string.
+
+- "X" means to avoid strumming the string at all or mute it. "O" means to strum the string open, which means don’t press it onto the fretboard. ]
 $image[1;https://cdn.discordapp.com/attachments/889976693102628904/921466604074704916/20211217_234850.jpg]
 $color[1;2f3135]
 $footer[1;Source - musicgrotto.com | 2 out of NaN availiable tips.]`                
