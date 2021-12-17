7ba60a1d09ae6698ae61032278e6030cfa9c94f3
lethiferal <95504439+lethiferal@users.noreply.github.com>
2021-12-17 17:57:56 +0530

    Create tips.js

---
+++ Commands/C3/tips.js
@@ -0,0 +1,22 @@
+ module.exports = [{
+    name:"tips",
+    aliases:["tip","cheats","cheat"],
+    code:`
+$if[$random[1;1;no]==1]
+$loop[1;{execute:progression}]
+
+`
+    },{
+  name:"progression",
+  type:"awaited",
+  code:`$author[1;Music Theory Advice;https://cdn.discordapp.com/attachments/889976693102628904/921376597313978378/20211217_175129.png]
+$title[1;Chord Progression Formulae] 
+$description[1;Chord progression formulae may appear to be a difficult math equation at first glance. However, they're not as difficult as you may believe. Roman numerals, which represent notes in a scale, are used to show these charts.
+It'll make coming up with root notes a lot easier. Keep in mind that these formulas are tools, not rules. You'll come up with chord progressions that sound really nice and natural if you follow these guidelines.]
+$addField[1;Major keys;fig. 1.2;yes]
+$addField[1;Minor keys;fig. 1.1;yes]
+$image[1;https://cdn.discordapp.com/attachments/889976693102628904/921374821542166528/20211217_174418.jpg]
+$color[1;2f3135]
+$footer[1;Source - musicnotes.com | 1 out of NaN availiable tips.]
+`
+}]
