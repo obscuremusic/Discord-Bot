582066c0b532d98a9c06ef77df5b7be556fb03bd
lethiferal <95504439+lethiferal@users.noreply.github.com>
2021-12-19 02:30:07 +0530

    Update qrcode.js

--- Commands/C1/qrcode.js
+++ Commands/C1/qrcode.js
@@ -7,6 +7,6 @@ aliases:["qr"],
 code:`$title[1;QR code Generated (360x360)]
 $image[1;https://api.qrserver.com/v1/create-qr-code/?size=360x360&data=$replaceText[$message; ;%20]]
 $color[1;2f3135]
-$footer[Source qrserver.com | $argsCount words.]
+$footer[1;Source qrserver.com | $argsCount words.]
 $onlyIf[$message!=;> :information_:**You need to provide a text!**]`
 }
