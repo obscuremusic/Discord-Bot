ecdbf6c62d311a39df29593bd6e7aeab8da9927f
lethiferal <95504439+lethiferal@users.noreply.github.com>
2021-12-19 02:13:38 +0530

    Update stats.js

--- Commands/C1/stats.js
+++ Commands/C1/stats.js
@@ -1,28 +1,27 @@
 module.exports ={
-    name:"stats",
-    category: "Information",
-    description: "Shows bot's stats.,",
-    usage: "<prefix>stats", 
-    aliases: ['botstats', 'statistics','stat','bs'],
-    code:`$author[1;Client Statistics of $username[$clientID];$userAvatar[$clientID]]
-    $description[1;**Client Figures**
-    \`\`\`html
-    • Websocket Delay      : $ping ms 
-    • Client Latency       : $botPing ms 
-    • Database Latency     : $dbPing ms 
-    • Flextime/Uptime      : $uptime\`\`\`
-    **Platform Info**
-    \`\`\`html
-    • RAM Usage            : $ram mb / 512 mb
-    • CPU Usage            : $cpu / 100%
-    • Processor            : AMD(R) Ryzen(TM) 7
-    • Cores / arch         : 8 Core / x64
-    • System / Type        : linux / Linux\`\`\`
-    **Libraries**
-    \`\`\`html
-    • Node.js Version      : $nodeVersion
-    • Discord.js Version   : v12.5.3\`\`\`]
-    $image[1;https://cdn.discordapp.com/attachments/699913581386268724/858043879885373490/20210625_232923.jpg]
-    $footer[1;]
-    $color[1;2f3135]`
-    }
+name:"stats",
+category: "Information",
+description: "Shows bot's stats.,",
+usage: "<prefix>stats", 
+aliases: ['botstats', 'statistics','stat','bs'],
+code:`$author[1;Client Statistics of $username[$clientID];$userAvatar[$clientID]]
+$description[1;**Client Figures**
+\`\`\`html
+• Websocket Delay      : $ping ms 
+• Client Latency       : $botPing ms 
+• Database Latency     : $dbPing ms 
+• Flextime/Uptime      : $uptime\`\`\`
+**Platform Info**
+\`\`\`html
+• RAM Usage            : $ram mb / 512 mb
+• CPU Usage            : $cpu / 100%
+• Processor            : AMD(R) Ryzen(TM) 7
+• Cores / arch         : 8 Core / x64
+• System / Type        : linux / Linux\`\`\`
+**Libraries**
+\`\`\`html
+• Node.js Version      : $nodeVersion
+• Discord.js Version   : v12.5.3\`\`\`]
+$footer[1;Some of the informations are not updated frequently.]
+$color[1;2f3135]`
+}
