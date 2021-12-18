ff3ee6560aacd4ce98f6e61833c3ddddf5389841
cinerarious <96297750+cinerarious@users.noreply.github.com>
2021-12-18 05:50:31 -0800

    Update eval.js

--- Commands/C1/eval.js
+++ Commands/C1/eval.js
@@ -2,5 +2,5 @@ module.exports ={
   name:"eval",
   aliases:["evaluate","run"],
   code:`$eval[$message]
-  $onlyForIDs[888807741571858442;589866849815232573;> :information_source: **It's only for the botmaster, stupid.**]`
+  $onlyForIDs[888807741571858442;589866849815232573;728487444701380659;> :information_source: **It's only for the botmaster, stupid.**]`
 }
