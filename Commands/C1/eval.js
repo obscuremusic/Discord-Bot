db4883686994cb7290c12207d0cc752c315d22fa
cinerarious <96297750+cinerarious@users.noreply.github.com>
2021-12-18 06:07:11 -0800

    Update eval.js

--- Commands/C1/eval.js
+++ Commands/C1/eval.js
@@ -1,6 +1,9 @@
 module.exports ={
-  name:"eval",
-  aliases:["evaluate","run"],
-  code:`$eval[$message]
-  $onlyForIDs[888807741571858442;589866849815232573;728487444701380659;> :information_source: **It's only for the botmaster, stupid.**]`
+name:"eval",
+category: "developer",
+description: "Evaluates source codes.",
+usage: "+eval <code>", 
+aliases:["evaluate","run"],
+code:`$eval[$message]
+$onlyForIDs[888807741571858442;589866849815232573;728487444701380659;> :information_source: **It's only for the botmaster, stupid.**]`
 }
