2024624c29b199fa33c9b2ee545815417adb60aa
Cinerarius <96297750+cinerarius@users.noreply.github.com>
2021-12-19 00:41:34 -0800

    Update ping.js

--- Commands/C4/ping.js
+++ Commands/C4/ping.js
@@ -1,7 +1,10 @@
 module.exports ={
-  name:"ping",
-  aliases:["latency","pong","delay"],
-  code:`> :information_source: **Collecting Ping...**
+name:"ping",
+category: "developer",
+description: "Returns the latency",
+usage: "+ping",
+aliases:["latency","pong","delay"],
+code:`> :information_source: **Collecting Ping...**
 $editIn[1s;> :ping_pong: **Pong!** \`$ping ms\`]
 $deleteIn[5s]
 $deletecommand`
