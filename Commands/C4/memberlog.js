1614f70648a532f8b1002eb6158142e1ffa577ce
lethiferal <95504439+lethiferal@users.noreply.github.com>
2021-12-20 02:26:23 +0530

    Update memberlog.js

--- Commands/C4/memberlog.js
+++ Commands/C4/memberlog.js
@@ -6,7 +6,7 @@ module.exports = [{
   type:"join",
   code:`$thumbnail[1;$authorAvatar]
   $author[1;$username joined.;https://cdn.discordapp.com/attachments/889976693102628904/922213724863987732/add-user.png]
-  $addFields[1;Username:\`\`\`kt\n$username\`\`\`:yes;User ID:\`\`\`kt\n$authorID\`\`\`:yes]
+  $addFields[1;Username:\`\`\`kt\n$username#$discriminator\`\`\`:yes;User ID:\`\`\`kt\n$authorID\`\`\`:yes]
   $addFields[1;Membership Date:\`\`\`kt\n$formatDate[$dateStamp;L], $hour#COLON#$minute#COLON#$second AM/PM (UTC)\`\`\`:yes;Creation Date:\`\`\`kt\n$creationDate[$authorID]\`\`\`:yes]
   $color[1;006400]
   `
@@ -18,7 +18,7 @@ module.exports = [{
   type:"leave",
   code:`$thumbnail[1;$authorAvatar]
   $author[1;$username left.;https://cdn.discordapp.com/attachments/889976693102628904/922213725090492456/remove-user.png]
-  $addFields[1;Username:\`\`\`kt\n$username\`\`\`:yes;User ID:\`\`\`kt\n$authorID\`\`\`:yes]
+  $addFields[1;Username:\`\`\`kt\n$username#$discriminator\`\`\`:yes;User ID:\`\`\`kt\n$authorID\`\`\`:yes]
   $addFields[1;Leave Date:\`\`\`kt\n$formatDate[$dateStamp;L], $hour#COLON#$minute#COLON#$second AM/PM (UTC)\`\`\`:yes;Creation Date:\`\`\`kt\n$creationDate[$authorID]\`\`\`:yes]
   $color[1;8B00000]`
 }]
