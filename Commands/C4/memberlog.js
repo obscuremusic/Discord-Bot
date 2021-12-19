89abc243556946c7782970b67cedb959adc1513d
lethiferal <95504439+lethiferal@users.noreply.github.com>
2021-12-20 02:12:03 +0530

    Update memberlog.js

--- Commands/C4/memberlog.js
+++ Commands/C4/memberlog.js
@@ -3,7 +3,7 @@ module.exports = [{
   name:"joinlog",
   category: "moderation",
   description: "Logs when someone joins.",
-  type:"join",
+  type:"guildJoin",
   code:`$author[1;$username joined.;https://cdn.discordapp.com/attachments/889976693102628904/922213724863987732/add-user.png]
   $addFields[1;Username:\`\`\`kt\n$username\`\`\`:yes;User ID:\`\`\`kt\n$authorID\`\`\`:yes]
   $addFields[1;Membership Date:\`\`\`kt\n$formatDate[$dateStamp;L], $hour:$minute:$second AM/PM (UTC)\`\`\`:yes;Creation Date:\`\`\`kt\n$creationDate[$authorID]\`\`\`:yes]
@@ -14,7 +14,7 @@ module.exports = [{
   name:"leavelog",
   category: "moderation",
   description: "Logs when someone leaves.",
-  type:"leave",
+  type:"guildLeave",
   code:`$author[1;$username left.;https://cdn.discordapp.com/attachments/889976693102628904/922213725090492456/remove-user.png]
   $addFields[1;Username:\`\`\`kt\n$username\`\`\`:yes;User ID:\`\`\`kt\n$authorID\`\`\`:yes]
   $addFields[1;Leave Date:\`\`\`kt\n$formatDate[$dateStamp;L], $hour:$minute:$second AM/PM (UTC)\`\`\`:yes;Creation Date:\`\`\`kt\n$creationDate[$authorID]\`\`\`:yes]
