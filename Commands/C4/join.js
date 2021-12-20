39bb2b833d081a7c840cc269950898ee0d2b6f02
lethiferal <95504439+lethiferal@users.noreply.github.com>
2021-12-21 01:54:49 +0530

    Update join.js

--- Commands/C4/join.js
+++ Commands/C4/join.js
@@ -5,8 +5,8 @@ module.exports = {
   description: "Logs when someone joins.",
   type:"join",
   code:`$thumbnail[1;$authorAvatar]
-  $author[1;$username#$discriminator joined.;https://cdn.discordapp.com/attachments/889976693102628904/922213724863987732/add-user.png]
+  $author[1;$username#$discriminator joined.;https://cdn.discordapp.com/attachments/889976693102628904/922584796356575233/20211221_015201.png]
   $addField[1;User ID;\`\`\`kt\n$authorID\`\`\`;yes]
-  $addFields[1;Membership Date:\`\`\`kt\n$formatDate[$dateStamp;L], $hour#COLON#$minute#COLON#$second AM/PM (UTC)\`\`\`:yes;Creation Date:\`\`\`kt\n$creationDate[$authorID]\`\`\`:yes]
+  $addFields[1;Membership Date:\`\`\`kt\n$formatDate[$dateStamp;L], $hour#COLON#$minute#COLON#$second AM/PM (UTC)\`\`\`:yes;Creation Date:\`\`\`kt\n$creationDate[$authorID] (UTC)\`\`\`:yes]
   $color[1;006400]`
 }
