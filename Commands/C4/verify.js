ffb56aa6e0268fb067d12e1cda5603f0bf542518
lethiferal <95504439+lethiferal@users.noreply.github.com>
2021-12-21 02:24:42 +0530

    Create verify.js

---
+++ Commands/C4/verify.js
@@ -0,0 +1,20 @@
+module.exports = [{
+name:"verify",
+category: "moderation",
+description: "Verifies the given member",
+usage: "-verify <user id>", 
+aliases:["confirm","validate"],
+code:`
+$sendMessage[{execute:verifiedstaffnotification};no]
+$sendDM[{execute:verifiedmembernotification};$findMember[$message];no]
+$giveRole[$findMember[$message];$getServerVar[membershiprole]]
+$onlyIf[$hasPerms[$authorID;kick]==true;> :information_source: **You don't have enough permissions.**]`
+},{
+  name:"verifiedmembernotification",
+  type:"awaited",
+  code:`test`
+},{
+  name:"verifiedstaffnotification",
+  type:"awaited",
+  code:`test`
+}]
