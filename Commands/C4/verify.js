995e079ece9a852a8493ec75e9d7be6ee0998e06
lethiferal <95504439+lethiferal@users.noreply.github.com>
2021-12-21 22:15:07 +0530

    Update verify.js

--- Commands/C4/verify.js
+++ Commands/C4/verify.js
@@ -6,14 +6,14 @@ usage: "-verify <user id>",
 aliases:["confirm","validate"],
 code:`$sendMessage[{execute:verifiedstaffnotification};no]
 $sendDM[{execute:verifiedmembernotification};$findMember[$message];no]
-$giveRole[$findMember[$message];$getServerVar[membershiprole]]
+$giveRole[$guildID;$findMember[$message];$getServerVar[membershiprole]]
 $onlyPerms[kick;> :information_source: **You don't have enough permissions.**]`
 },{
   name:"verifiedmembernotification",
   type:"awaited",
-  code:`test`
+  code:`member notification is working`
 },{
   name:"verifiedstaffnotification",
   type:"awaited",
-  code:`test`
+  code:`verification working`
 }]
