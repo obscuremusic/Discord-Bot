4221afa1a827fcc4f8f21da7ea2e0ee3245e0c51
lethiferal <95504439+lethiferal@users.noreply.github.com>
2021-12-22 00:17:55 +0530

    Update cleverbot.js

--- Commands/C5/cleverbot.js
+++ Commands/C5/cleverbot.js
@@ -1,6 +1,6 @@
 module.exports ={
 name:"$alwaysExecute",
-code:`$replaceText[$jsonRequest[https://api.affiliateplus.xyz/api/chatbot?message=$message&botname=UMC%20ChatBot&ownername=Lethiferal&build=beta&celebrity=Varg+Vikernes&city=Kolkata&country=India&email=none&company=UMC&favoriteband=Navjarmaahr&favoritecolor=black&favoriteseason=winter&favoritesong=Beyond+Tears+by+Oppressive+Light&kindmusic=DSBM&location=Server+Farm&user=$authorID;message;> :information_source: **An error occured**];$authorID;$username[$authorID]]
+code:`$replaceText[$jsonRequest[https://api.affiliateplus.xyz/api/chatbot?message=$message&botname=UMC%20ChatBot&ownername=Lethiferal&build=beta&celebrity=Varg+Vikernes&city=Kolkata&country=India&email=none&company=UMC&favoriteband=Navjarmaahr&favoritecolor=black&favoriteseason=winter&favoritesong=Beyond+Tears+by+Oppressive+Light&kindmusic=black+metal&location=Server+Farm&user=$authorID;message;> :information_source: **An error occured**];$authorID;$username[$authorID]]
 $reply[$messageID;no]
 $botTyping
 $onlyIf[$channelID==$getServerVar[chatbotchannel];]`
