6c120a6a79661a3cd35cce0c5f7b46427aa1833c
Cinerarius <96297750+cinerarius@users.noreply.github.com>
2021-12-22 05:43:55 -0800

    Update tips.js

--- Commands/C3/tips.js
+++ Commands/C3/tips.js
@@ -1,12 +1,8 @@
  module.exports = [{
     name:"tips",
     aliases:["tip","cheats","cheat"],
-    code:`$if[$get[tip]==1]
-    $sendMessage[{execute:progression};no]
-    $elseIf[$get[tip]==2]
-    $sendMessage[{execute:guitarchord};no]]
-    $endelseif
-    $endif
+    code:`$sendMessage[{execute:progression};no]
+    $sendMessage[{execute:guitarchord};no]
     $let[tip;$random[1;2;no]]`
 },{
 name:"progression",
@@ -19,23 +15,17 @@ $addField[1;Major keys;fig. 1.2;yes]
 $addField[1;Minor keys;fig. 1.1;yes]
 $image[1;https://cdn.discordapp.com/attachments/889976693102628904/921374821542166528/20211217_174418.jpg]
 $color[1;2f3135]
-$footer[1;Source - musicnotes.com | 1 out of NaN availiable tips.]`
+$footer[1;Source - musicnotes.com | 1 out of NaN availiable tips.]
+$onlyIf[$get[tip]==1;]`
 },{
 name:"guitarchord",
 type:"awaited",
 code:`$author[1;Music Theory Advice;https://cdn.discordapp.com/attachments/889976693102628904/921376597313978378/20211217_175129.png]
 $title[1;Interpreting Guitar Chord Charts] 
-$description[1;Three or more separate notes are played together to form a chord. An interval, often known as a double-stop, is two separate notes played together.
-
-- Your guitar strings are represented by the vertical lines on the guitar chart. Because the chart is a vertical display and you're gazing down at your instrument horizontally, it might be difficult for novices to get the feel of it.
-
-- The horizontal lines on your guitar's neck symbolise the fret bars.
-The dots indicate where your fingers should be placed on the fretboard. They're positioned on a vertical line to indicate the correct string, and inside the fret boxes to indicate which fret to press for that string. If there isn't a dot on the fretboard, that signifies you shouldn't touch that string.
-
-- Sometimes there will be numbers in the dots, or numbers will be seen beneath the chart.Each of your fingers is represented by a number. A number next to a vertical line indicates which finger should be used on that string.
-
-- "X" means to avoid strumming the string at all or mute it. "O" means to strum the string open, which means don’t press it onto the fretboard. ]
+$description[1;Three or more separate notes are played together to form a chord. An interval, often known as a double-stop, is two separate notes played together. Your guitar strings are represented by the vertical lines on the guitar chart. Because the chart is a vertical display and you're gazing down at your instrument horizontally, it might be difficult for novices to get the feel of it. The horizontal lines on your guitar's neck symbolise the fret bars.
+The dots indicate where your fingers should be placed on the fretboard. They're positioned on a vertical line to indicate the correct string, and inside the fret boxes to indicate which fret to press for that string. If there isn't a dot on the fretboard, that signifies you shouldn't touch that string. Sometimes there will be numbers in the dots, or numbers will be seen beneath the chart.Each of your fingers is represented by a number. A number next to a vertical line indicates which finger should be used on that string. "X" means to avoid strumming the string at all or mute it. "O" means to strum the string open, which means don’t press it onto the fretboard. ]
 $image[1;https://cdn.discordapp.com/attachments/889976693102628904/921466604074704916/20211217_234850.jpg]
 $color[1;2f3135]
-$footer[1;Source - musicgrotto.com | 2 out of NaN availiable tips.]`                
+$footer[1;Source - musicgrotto.com | 2 out of NaN availiable tips.]
+$onlyIf[$get[tip]==2;]`                
 }]
