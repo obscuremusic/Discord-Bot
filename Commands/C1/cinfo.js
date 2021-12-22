ac13ee33dc7ae4ed5c630052d021ed118ddbb504
lethiferal <95504439+lethiferal@users.noreply.github.com>
2021-12-22 23:15:43 +0530

    Update cinfo.js

--- Commands/C1/cinfo.js
+++ Commands/C1/cinfo.js
@@ -6,10 +6,10 @@ usage: "+help <command>",
 aliases:["help","commandinfo"],
 code:`$author[1;Information about "$message[1]" command ;https://cdn.discordapp.com/attachments/889976693102628904/921376597313978378/20211217_175129.png]
 $title[1;Command - $message[1]]
-$addField[1;Description;$commandInfo[$message[1];description];no]
-$addField[1;Usage;$commandInfo[$message[1];usage];yes]
-$addField[1;Aliases;$commandInfo[$message[1];aliases];yes]
-$addField[1;Category;$commandInfo[$message[1];category];yes]
+$addField[1;Description;\`\`\`fix\n$commandInfo[$message[1];description]\`\`\`;no]
+$addField[1;Usage;\`\`\`fix\n$commandInfo[$message[1];usage]\`\`\`;yes]
+$addField[1;Aliases;\`\`\`fix\n$commandInfo[$message[1]\`\`\`;aliases];yes]
+$addField[1;Category;\`\`\`fix\n$commandInfo[$message[1]\`\`\`;category];yes]
 $footer[1;Requested by $username[$authorID]]
 $color[1;000000]
 $onlyIf[$message[1]!=;]`
