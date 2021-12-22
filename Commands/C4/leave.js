5de1c8eb48c8b39a99d5690d1ca2f0d0f4a47fa8
lethiferal <95504439+lethiferal@users.noreply.github.com>
2021-12-22 23:16:14 +0530

    Update leave.js

--- Commands/C4/leave.js
+++ Commands/C4/leave.js
@@ -6,7 +6,7 @@ module.exports = {
   type:"leave",
   code:`$thumbnail[1;$authorAvatar]
   $author[1;$username#$discriminator left.;https://cdn.discordapp.com/attachments/889976693102628904/922584796700487730/20211221_015222.png]
-  $addField[1;User ID;\`\`\`kt\n$authorID\`\`\`;yes]
-  $addFields[1;Leave Date:\`\`\`kt\n$formatDate[$dateStamp;L], $hour#COLON#$minute#COLON#$second AM/PM (UTC)\`\`\`:yes;Creation Date:\`\`\`kt\n$creationDate[$authorID] (UTC)\`\`\`:yes]
+  $addField[1;User ID;\`\`\`fix\n$authorID\`\`\`;yes]
+  $addFields[1;Leave Date:\`\`\`fix\n$formatDate[$dateStamp;L], $hour#COLON#$minute#COLON#$second AM/PM (UTC)\`\`\`:yes;Creation Date:\`\`\`fix\n$creationDate[$authorID] (UTC)\`\`\`:yes]
   $color[1;8B0000]`
 }
