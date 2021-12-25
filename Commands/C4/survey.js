e54040fb973a6455a8858c7b0f9de53a5c228f25
lethiferal <95504439+lethiferal@users.noreply.github.com>
2021-12-26 01:43:17 +0530

    Update survey.js

--- Commands/C4/survey.js
+++ Commands/C4/survey.js
@@ -8,11 +8,10 @@ $image[1;https://cdn.discordapp.com/attachments/889976693102628904/9243689550769
 $color[1;2f3135]
 $title[2;［⎋］︰𝖁erification]
 $description[2;\`\`\`fix
-Since we're a private server, it is important that we keep track of who gets access. As a solution, we have built a verification system to prevent nefarious and dimwitted individuals from gaining access to the server. Please select one of the options below to continue. If you have any problems, please submit a ticket.\`\`\`]
+Since we're a private server, it is important that we keep track of who gets access. As a solution, we have built a verification system to prevent nefarious and dimwitted individuals from gaining access to the server. Please select one of the options below to continue. If the bot fails to respond to the interaction, please use the second option. If you have any problems, please submit a ticket.\`\`\`]
 $image[2;https://cdn.discordapp.com/attachments/590020114234081280/759371484287860766/20200926_164101.jpg]
 $color[2;2f3135]
-$addbutton[1;Discord Survey;3;verify_vd;no]
-$addbutton[2;Form Submit;5;https://forms.gle/L8VjMVexgVG4C8gN8;no]
-$onlyPerms[manageserver;> :information_source: **You don't have enough permissions.**]
-`
+$addbutton[1;Discord Survey;1;verify_vd;no;<:icon_discord:924389666495557693>]
+$addbutton[2;Form Submit;5;https://forms.gle/L8VjMVexgVG4C8gN8;no;<:icon_gforms:924389619326402581>]
+$onlyPerms[manageserver;> :information_source: **You don't have enough permissions.**]`
 }
