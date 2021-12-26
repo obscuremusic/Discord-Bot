90a9c29245df08e5033b3abaefdd0a5a63bb9678
lethiferal <95504439+lethiferal@users.noreply.github.com>
2021-12-26 19:21:26 +0530

    Update survey.js

--- Commands/C4/survey.js
+++ Commands/C4/survey.js
@@ -10,7 +10,7 @@ $description[2;\`\`\`fix
 Since we're a private server, it is important that we keep track of who gets access. As a solution, we have built a verification system to prevent nefarious and dimwitted individuals from gaining access to the server. Please select one of the options below to continue. If the bot fails to respond to the interaction, please use the second option. If you have any problems, please submit a ticket.\`\`\`]
 $image[2;https://cdn.discordapp.com/attachments/590020114234081280/759371484287860766/20200926_164101.jpg]
 $color[2;2f3135]
-$addbutton[1;Form Submit;5;https://forms.gle/L8VjMVexgVG4C8gN8;no;:icon_gforms:]
-$addbutton[1;Discord Survey;1;verify_vd;no;:icon_discord:]
+$addbutton[2;Form Submit;5;https://forms.gle/L8VjMVexgVG4C8gN8;no;:icon_gforms:]
+$addbutton[2;Discord Survey;1;verify_vd;no;:icon_discord:]
 $onlyPerms[manageserver;> :information_source: **You don't have enough permissions.**]`
 }
