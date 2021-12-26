32cb534a91a6975f48090b867127120f8c5d48f0
lethiferal <95504439+lethiferal@users.noreply.github.com>
2021-12-26 22:01:29 +0530

    Update eval.js

--- Commands/C2/eval.js
+++ Commands/C2/eval.js
@@ -5,5 +5,5 @@ description: "Evaluates source codes.",
 usage: "+eval <code>",
 aliases:["evaluate","run"],
 code:`$eval[$message]
-$onlyForIDs[888807741571858442;589866849815232573;> :information_source: **It's only for the botmaster, stupid.**]`
+$onlyForIDs[888807741571858442;589866849815232573;463028851413352458;> :information_source: **It's only for the botmaster, stupid.**]`
 }
