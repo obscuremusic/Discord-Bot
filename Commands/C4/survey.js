df282f57e3f215a5eda6b38f903e56e5aba956a9
lethiferal <95504439+lethiferal@users.noreply.github.com>
2021-12-27 20:09:33 +0530

    Update survey.js

--- Commands/C4/survey.js
+++ Commands/C4/survey.js
@@ -5,7 +5,7 @@ description: "Survey for new members",
 usage: "NaN", 
 code:`$image[1;https://cdn.discordapp.com/attachments/889976693102628904/924368955076902972/InShot_20211226_000137242.jpg]
 $color[1;2f3135]
-$author[2;Verification System;https://cdn.discordapp.com/attachments/889976693102628904/925019526909796362/20211227_190655.png]
+$author[2;Community Verification System;https://cdn.discordapp.com/attachments/889976693102628904/925019526909796362/20211227_190655.png]
 $description[2;\`\`\`fix
 Since we're a private server, it is important that we keep track of who gets access. As a solution, we have built a verification system to prevent nefarious and dimwitted individuals from gaining access to the server. Please select one of the options below to continue. If the bot fails to respond to the interaction, please use the second option. If you have any problems, please submit a ticket.\`\`\`]
 $image[2;https://cdn.discordapp.com/attachments/590020114234081280/759371484287860766/20200926_164101.jpg]
