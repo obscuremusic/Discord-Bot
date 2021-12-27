70237e8c286c8e914e4c40c05157a1cffd16fbcf
lethiferal <95504439+lethiferal@users.noreply.github.com>
2021-12-27 19:10:57 +0530

    Update survey.js

--- Commands/C4/survey.js
+++ Commands/C4/survey.js
@@ -1,11 +1,11 @@
-module.exports ={
+module.exports = [{
 name:"survey",
 category: "moderation",
 description: "Survey for new members",
 usage: "NaN", 
 code:`$image[1;https://cdn.discordapp.com/attachments/889976693102628904/924368955076902972/InShot_20211226_000137242.jpg]
 $color[1;2f3135]
-$title[2;［⎋］︰𝖁erification]
+$author[2;Verification System;https://cdn.discordapp.com/attachments/889976693102628904/925019526909796362/20211227_190655.png]
 $description[2;\`\`\`fix
 Since we're a private server, it is important that we keep track of who gets access. As a solution, we have built a verification system to prevent nefarious and dimwitted individuals from gaining access to the server. Please select one of the options below to continue. If the bot fails to respond to the interaction, please use the second option. If you have any problems, please submit a ticket.\`\`\`]
 $image[2;https://cdn.discordapp.com/attachments/590020114234081280/759371484287860766/20200926_164101.jpg]
@@ -13,4 +13,4 @@ $color[2;2f3135]
 $addbutton[1;Form Submit;5;https://forms.gle/L8VjMVexgVG4C8gN8;no;<:icon_gforms:924389619326402581>]
 $addbutton[1;Discord Survey;1;verify_vd;no;<:icon_discord:924389666495557693>]
 $onlyPerms[manageserver;> :information_source: **You don't have enough permissions.**]`
-}
+}]
