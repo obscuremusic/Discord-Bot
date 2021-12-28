bb927b4c038eb9593db838c06846016175145cb5
lethiferal <95504439+lethiferal@users.noreply.github.com>
2021-12-28 19:50:36 +0530

    Update qrcode.js

--- Commands/C1/qrcode.js
+++ Commands/C1/qrcode.js
@@ -6,7 +6,8 @@ usage: "+qrcode <text>",
 aliases:["qr"],
 code:`$author[1;QR code Generated (360x360);https://cdn.discordapp.com/attachments/889976693102628904/925390021089964202/20211228_193853.png]
 $description[1;**[Image Link](https://api.qrserver.com/v1/create-qr-code/?size=360x360&data=$replaceText[$message; ;%20]) | [QR Service](https://qrserver.com)**
-\`\`\`$message\`\`\`]
+\`\`\`fix
+$message\`\`\`]
 $image[1;https://api.qrserver.com/v1/create-qr-code/?size=360x360&data=$uri[encode;$message]]
 $color[1;2f3135]
 $footer[1;Source - qrserver.com | $argsCount words.]
