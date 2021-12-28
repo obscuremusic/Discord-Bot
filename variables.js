92adcb36cfe7a365d538c0156701c1435928e287
lethiferal <95504439+lethiferal@users.noreply.github.com>
2021-12-28 20:13:37 +0530

    Update variables.js

--- variables.js
+++ variables.js
@@ -1,7 +1,7 @@
 const variables = {
  chatbotchannel: "759309051287830558",
  joinlogchannel: "852198467006103602",
- verifycategory: "852198467006103602",
+ verifycategory: "759465031736754226",
  membershiprole: "759143785417539634",
 };
 module.exports = variables;
