3228dcb8e4eecba9391f456fdc55cc9d9f0f18d1
lethiferal <95504439+lethiferal@users.noreply.github.com>
2021-12-28 21:30:48 +0530

    Update survey.js

--- Commands/C4/survey.js
+++ Commands/C4/survey.js
@@ -19,5 +19,6 @@ $onlyPerms[manageserver;> :information_source: **You don't have enough permissio
   prototype: 'button',
   code:`$interactionReply[> :information_source: **Your application was successfully created at <#$findChannel[➥・［⎋］﹕$username[$findMember[$message[1]]]．;yes]>.**;;;;yes]
   $channelSendMessage[$findChannel[➥・［⎋］﹕$username[$findMember[$message[1]]]．;yes];ok;no]
-  $newTicket[➥・［⎋］﹕$username[$authorID]．;;$getServerVar[verifycategory];no;]`
+  $newTicket[➥・［⎋］﹕$username[$authorID]．;;$getServerVar[verifycategory];no;]
+  $cooldown[2h]`
 }]
