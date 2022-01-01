c6819399235012ce9012d33afee330923eede72f
lethiferal <95504439+lethiferal@users.noreply.github.com>
2022-01-01 21:56:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 const loadFunction = require("./Functions/Handler/handler.js");
 const variables = require("./variables")
-const aoijs = require('aoi.js');
+const Discord = require('aoi.js');
 
-const client1 = new aoijs.Bot({
+const client1 = new Discord.Bot({
    token: process.env['CT1'],
    prefix: ["+","<@$clientID>","<@!$clientID>"],
    intents: "all"
  });
 
- const client2 = new aoijs.Bot({
+ const client2 = new Discord.Bot({
    token: process.env['CT2'],
    prefix: ["~","<@$clientID>","<@!$clientID>"],
    intents: "all"
  });
  
- const client3 = new aoijs.Bot({
+ const client3 = new Discord.Bot({
    token: process.env['CT3'],
    prefix: ["%","<@$clientID>","<@!$clientID>"],
    intents: "all"
  });
  
-  const client4 = new aoijs.Bot({
+  const client4 = new Discord.Bot({
    token: process.env['CT4'],
    prefix: ["-","<@$clientID>","<@!$clientID>"],
    intents: "all"
  });
 
-  const client5 = new aoijs.Bot({
+  const client5 = new Discord.Bot({
    token: process.env['CT5'],
    prefix: ["=","<@$clientID>","<@!$clientID>"],
    intents: "all"
  });
 
-const LavalinkCT1 = new aoijs.Lavalink(client1);
+const LavalinkCT1 = new Discord.Lavalink(client1);
 LavalinkCT1.addNode({
     url: "lava.link:80",
     password: "lavalink",
@@ -40,7 +40,7 @@ LavalinkCT1.addNode({
     secure: false
 })
 
-const LavalinkCT2 = new aoijs.Lavalink(client2);
+const LavalinkCT2 = new Discord.Lavalink(client2);
 LavalinkCT2.addNode({
     url: "lava.link:80",
     password: "lavalink",
@@ -48,7 +48,7 @@ LavalinkCT2.addNode({
     secure: false
 })
 
-const LavalinkCT3 = new aoijs.Lavalink(client3);
+const LavalinkCT3 = new Discord.Lavalink(client3);
 LavalinkCT3.addNode({
     url: "lava.link:80",
     password: "lavalink",
@@ -56,7 +56,7 @@ LavalinkCT3.addNode({
     secure: false
 })
 
-const LavalinkCT4 = new aoijs.Lavalink(client4);
+const LavalinkCT4 = new Discord.Lavalink(client4);
 LavalinkCT4.addNode({
     url: "lava.link:80",
     password: "lavalink",
@@ -64,7 +64,7 @@ LavalinkCT4.addNode({
     secure: false
 })
 
-const LavalinkCT5 = new aoijs.Lavalink(client5);
+const LavalinkCT5 = new Discord.Lavalink(client5);
 LavalinkCT5.addNode({
     url: "lava.link:80",
     password: "lavalink",
@@ -180,13 +180,13 @@ client5.readyCommand({ //Makes a console log whenever UMC Custom Bot (5)#7525 is
     code: `$log[Client 5 is online.]`
 })
 
-const clientloader_1 = new aoijs.LoadCommands(client1);
+const clientloader_1 = new Discord.LoadCommands(client1);
 clientloader_1.load(client1.cmd,'./Commands/C1/') 
-const clientloader_2 = new aoijs.LoadCommands(client2);
+const clientloader_2 = new Discord.LoadCommands(client2);
 clientloader_2.load(client2.cmd,'./Commands/C2/') 
-const clientloader_3 = new aoijs.LoadCommands(client3);
+const clientloader_3 = new Discord.LoadCommands(client3);
 clientloader_3.load(client3.cmd,'./Commands/C3/') 
-const clientloader_4 = new aoijs.LoadCommands(client4);
+const clientloader_4 = new Discord.LoadCommands(client4);
 clientloader_4.load(client4.cmd,'./Commands/C4/') 
-const clientloader_5 = new aoijs.LoadCommands(client5);
+const clientloader_5 = new Discord.LoadCommands(client5);
 clientloader_5.load(client5.cmd,'./Commands/C5/') 
