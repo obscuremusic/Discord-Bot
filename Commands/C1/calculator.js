93600833ce86481c79514108c5a541f4de378cef
lethiferal <95504439+lethiferal@users.noreply.github.com>
2022-01-02 01:31:24 +0530

    Update calculator.js

--- Commands/C1/calculator.js
+++ Commands/C1/calculator.js
@@ -4,12 +4,13 @@ category: "utility",
 description: "An handy button Calculator.",
 usage: "+calculator", 
 aliases:["cal","calculate"],
-code:`$djsEval[
+code:`$djsEval[(async () => {
 const discordjs = require('discord.js')
 const simplydjs = require('simply-djs')
+const client = global.C1
 simplydjs.calculator(message, {
   embedColor: "#075FFF"
   credit: "false"
-});
-]`
+})
+})()]`
 }
