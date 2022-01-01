172d7ee212cbc14ca84c277389484af77fca5279
lethiferal <95504439+lethiferal@users.noreply.github.com>
2022-01-01 21:12:49 +0530

    Update event.js

--- Commands/C1/event.js
+++ Commands/C1/event.js
@@ -11,108 +11,131 @@ $if[$message==Youtube;Error]
  Starting Youtube $djseval[
 const { DiscordTogether } = require('discord-together');
 
-client1.discordTogether = new DiscordTogether(client);
-client1.discordTogether.createTogetherCode('$voiceID', 'youtube').then(async invite => {
+client.discordTogether = new DiscordTogether(client);
+client.discordTogether.createTogetherCode('$voiceID', 'youtube').then(async invite => {
  return message.channel.send(invite.code);
  });
  ]
 $endif
 $if[$message==Poker;Error]
 Starting Poker $djseval[
+const client = new aoijs.Bot({
+   token: process.env['CT1']
+ })
 const { DiscordTogether } = require('discord-together');
-
-client1.discordTogether = new DiscordTogether(client);
-client1.discordTogether.createTogetherCode('$voiceID', 'poker').then(async invite => {
+client.discordTogether = new DiscordTogether(client);
+client.discordTogether.createTogetherCode('$voiceID', 'poker').then(async invite => {
  return message.channel.send(invite.code);
  });
  ]
 $endif
 $if[$message==Betrayal;Error]
 Starting Betrayal $djseval[
+const client = new aoijs.Bot({
+   token: process.env['CT1']
+ })
 const { DiscordTogether } = require('discord-together');
-
-client1.discordTogether = new DiscordTogether(client);
-client1.discordTogether.createTogetherCode('$voiceID', 'betrayal').then(async invite => {
+client.discordTogether = new DiscordTogether(client);
+client.discordTogether.createTogetherCode('$voiceID', 'betrayal').then(async invite => {
  return message.channel.send(invite.code);
  });
  ]
  $endif
 $if[$message==Chess;Error]
 Starting Chess $djseval[
+const client = new aoijs.Bot({
+   token: process.env['CT1']
+ })
 const { DiscordTogether } = require('discord-together');
-
-client1.discordTogether = new DiscordTogether(client);
-client1.discordTogether.createTogetherCode('$voiceID', 'chess').then(async invite => {
+client.discordTogether = new DiscordTogether(client);
+client.discordTogether.createTogetherCode('$voiceID', 'chess').then(async invite => {
  return message.channel.send(invite.code);
  });
  ] 
  $endif
  $if[$message==Fishington;Error]
  Starting Fishington $djseval[
+const client = new aoijs.Bot({
+   token: process.env['CT1']
+ })
 const { DiscordTogether } = require('discord-together');
-
-client1.discordTogether = new DiscordTogether(client);
-client1.discordTogether.createTogetherCode('$voiceID', 'fishing').then(async invite => {
+client.discordTogether = new DiscordTogether(client);
+client.discordTogether.createTogetherCode('$voiceID', 'fishing').then(async invite => {
  return message.channel.send(invite.code);
  });
  ]
  $endif
  $if[$message==Lettertile;Error]
 Starting Letter tile $djseval[
+const client = new aoijs.Bot({
+   token: process.env['CT1']
+ })
 const { DiscordTogether } = require('discord-together');
-
-client1.discordTogether = new DiscordTogether(client);
-client1.discordTogether.createTogetherCode('$voiceID', 'lettertile').then(async invite => {
+client.discordTogether = new DiscordTogether(client);
+client.discordTogether.createTogetherCode('$voiceID', 'lettertile').then(async invite => {
  return message.channel.send(invite.code);
  });
  ]
  $endif
  $if[$message==Wordsnack;Error]
 Starting Word snack $djseval[
+const client = new aoijs.Bot({
+   token: process.env['CT1']
+ })
 const { DiscordTogether } = require('discord-together');
-
-client1.discordTogether = new DiscordTogether(client);
-client1.discordTogether.createTogetherCode('$voiceID', 'wordsnack').then(async invite => {
+client.discordTogether = new DiscordTogether(client);
+client.discordTogether.createTogetherCode('$voiceID', 'wordsnack').then(async invite => {
  return message.channel.send(invite.code);
  });
  ]
  $endif
  $if[$message==Doodlecrew;Error]
 Starting Doodle crew $djseval[
+const client = new aoijs.Bot({
+   token: process.env['CT1']
+ })
 const { DiscordTogether } = require('discord-together');
-
-client1.discordTogether = new DiscordTogether(client);
-client1.discordTogether.createTogetherCode('$voiceID', 'doodlecrew').then(async invite => {
+client.discordTogether = new DiscordTogether(client);
+client.discordTogether.createTogetherCode('$voiceID', 'doodlecrew').then(async invite => {
  return message.channel.send(invite.code);
  });
  ] 
  $endif
  $if[$message==Spellcast;error]
 Starting Spell cast $djseval[
+const aoijs = require('aoi.js');
+const client = new aoijs.Bot({
+   token: process.env['CT1']
+ })
 const { DiscordTogether } = require('discord-together');
-
-client1.discordTogether = new DiscordTogether(client);
-client1.discordTogether.createTogetherCode('$voiceID', 'spellcast').then(async invite => {
+client.discordTogether = new DiscordTogether(client);
+client.discordTogether.createTogetherCode('$voiceID', 'spellcast').then(async invite => {
  return message.channel.send(invite.code);
  });
  ]
  $endif
  $if[$message==Awkword;Error]
 Starting Awkword $djseval[
+const aoijs = require('aoi.js');
+const client = new aoijs.Bot({
+   token: process.env['CT1']
+ })
 const { DiscordTogether } = require('discord-together');
-
-client1.discordTogether = new DiscordTogether(client);
-client1.discordTogether.createTogetherCode('$voiceID', 'awkword').then(async invite => {
+client.discordTogether = new DiscordTogether(client);
+client.discordTogether.createTogetherCode('$voiceID', 'awkword').then(async invite => {
  return message.channel.send(invite.code);
  });
  ]
  $endif
  $if[$message==Puttyparty;error]
 Starting putty party $djseval[
+const aoijs = require('aoi.js');
+const client = new aoijs.Bot({
+   token: process.env['CT1']
+ })
 const { DiscordTogether } = require('discord-together');
-
-client1.discordTogether = new DiscordTogether(client);
-client1.discordTogether.createTogetherCode('$voiceID', 'puttyparty').then(async invite => {
+client.discordTogether = new DiscordTogether(client);
+client.discordTogether.createTogetherCode('$voiceID', 'puttyparty').then(async invite => {
  return message.channel.send(invite.code);
  });
  ]
