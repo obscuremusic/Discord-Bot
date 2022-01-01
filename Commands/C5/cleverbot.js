9b9de56a09e932464e2578aeb250c971117cbf12
lethiferal <95504439+lethiferal@users.noreply.github.com>
2022-01-01 21:29:18 +0530

    Update cleverbot.js

--- Commands/C5/cleverbot.js
+++ Commands/C5/cleverbot.js
@@ -1,6 +1,11 @@
 module.exports ={
 name:"$alwaysExecute",
-code:`$replaceText[$jsonRequest[https://api.affiliateplus.xyz/api/chatbot?message=$message&botname=UMC%20ChatBot&ownername=Lethiferal&build=beta&celebrity=Varg+Vikernes&city=Kolkata&country=India&email=none&company=UMC&favoriteband=Navjarmaahr&favoritecolor=black&favoriteseason=winter&favoritesong=Beyond+Tears+by+Oppressive+Light&kindmusic=black+metal&location=Server+Farm&user=$authorID;message;> :information_source: **An error occured**];$authorID;$username[$authorID]]
+code:`$djsEval[const simplydjs = require("simply-djs")
+simplydjs.chatbot(client, message, {
+ chid: "$getServerVar[chatbotchannel]"
+ name: "UMC Chatbot"
+ developer: "Lethiferal"
+});]
 $reply[$messageID;no]
 $botTyping
 $onlyIf[$channelID==$getServerVar[chatbotchannel];]`
