de236cbd7f2029ec7e09c6c23665ed39c65e4a60
Lethiferal <95504439+lethiferal@users.noreply.github.com>
2022-01-04 00:19:11 +0530

    Update tipspage.js

--- Commands/C3/tipspage.js
+++ Commands/C3/tipspage.js
@@ -39,9 +39,9 @@ simplydjs.embedPages(client, message, pages, {
   forwardEmoji: "927629508096454717",
   lastEmoji: "927629535850143855",
 
-  btncolor: "colors",
-  delcolor: "colors",
-  skipcolor: "colors",
+  btncolor: "primary",
+  delcolor: "secondary",
+  skipcolor: "primary",
 
   skipBtn: true,
   delBtn: true
