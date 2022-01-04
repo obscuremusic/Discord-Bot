698c42dc873b87303f4353a207a6911a2c83621c
Lethiferal <95504439+lethiferal@users.noreply.github.com>
2022-01-04 18:27:55 +0530

    Update tipspage.js

--- Commands/C3/tipspage.js
+++ Commands/C3/tipspage.js
@@ -15,7 +15,7 @@ let tip2 = new MessageEmbed().setTitle("Page 2");
 let pages = [tip1, tip2]
 
 simplydjs.embedPages(client, message, pages, {
-  firstEmoji: "927629391129903154",
+  firstEmoji: "927629421853179914",
   backEmoji: "927629391129903154",
   delEmoji: "927629461095067669",
   forwardEmoji: "927629508096454717",
