cbb0a6cc3b7c35c69c51811949a4c35dd32982af
Cinerarius <96297750+cinerarius@users.noreply.github.com>
2022-01-04 08:33:11 -0800

    Update tipspage.js

--- Commands/C3/tipspage.js
+++ Commands/C3/tipspage.js
@@ -11,14 +11,15 @@ const simplydjs = require('simply-djs');
 
 let tip1 = new MessageEmbed()
   .setAuthor({ name: 'Music Theory Advice', iconURL: 'https://cdn.discordapp.com/attachments/889976693102628904/921376597313978378/20211217_175129.png'})
+  .setFooter('Source - musicnotes.com \u007C \u25CF\u25CB');
   .setColor('#2f3135')
-  .setDescription('Chord progression formulae may appear to be a difficult math equation at first glance. However, they are not as difficult as you may believe. Roman numerals, which represent notes in a scale, are used to show these charts.\r\nIt will make coming up with root notes a lot easier. Keep in mind that these formulas are tools, not rules. You will come up with chord progressions that sound really nice and natural if you follow these guidelines.')
-
+  
+  
 let tip2 = new MessageEmbed()
   .setAuthor({ name: 'Music Theory Advice', iconURL: 'https://cdn.discordapp.com/attachments/889976693102628904/921376597313978378/20211217_175129.png'})
+  .setFooter('Source - musicnotes.com \u007C \u25CB\u25CF');
   .setColor('#2f3135')
-  .setDescription('Chord progression formulae may appear to be a difficult math equation at first glance. However, they are not as difficult as you may believe. Roman numerals, which represent notes in a scale, are used to show these charts.\r\nIt will make coming up with root notes a lot easier. Keep in mind that these formulas are tools, not rules. You will come up with chord progressions that sound really nice and natural if you follow these guidelines.')
-
+  
 let pages = [tip1, tip2];
 
 simplydjs.embedPages(client, message, pages, {
