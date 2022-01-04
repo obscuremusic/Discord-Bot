8cb690952e6ad3709ad29b099d66d30349785b98
Lethiferal <95504439+lethiferal@users.noreply.github.com>
2022-01-04 17:43:23 +0530

    Update tipspage.js

--- Commands/C3/tipspage.js
+++ Commands/C3/tipspage.js
@@ -8,29 +8,7 @@ code:`$djsEval[(async () => {
 const { MessageEmbed } = require('discord.js');
 const simplydjs = require('simply-djs');
 
-let embed1 = new MessageEmbed()
-	.setColor('#2f3135')
-	.setAuthor({ name: 'Music Theory Advice', iconURL: 'https://cdn.discordapp.com/attachments/889976693102628904/921376597313978378/20211217_175129.png'})
-	.setDescription('Chord progression formulae may appear to be a difficult math equation at first glance. However, they're not as difficult as you may believe. Roman numerals, which represent notes in a scale, are used to show these charts.\r\nIt'll make coming up with root notes a lot easier. Keep in mind that these formulas are tools, not rules. You'll come up with chord progressions that sound really nice and natural if you follow these guidelines.')
-	.addFields(
-		{ name: 'Major keys', value: 'fig. 1.1', inline: true },
-		{ name: 'Minor keys', value: 'fig. 1.2', inline: true },
-	)
-	.setImage('https://cdn.discordapp.com/attachments/889976693102628904/921374821542166528/20211217_174418.jpg')
-	.setFooter('Source - musicnotes.com | \u25CF\u25CB');
-  
-let embed2 = new MessageEmbed()
-	.setColor('#2f3135')
-	.setAuthor({ name: 'Music Theory Advice', iconURL: 'https://cdn.discordapp.com/attachments/889976693102628904/921376597313978378/20211217_175129.png'})
-	.setDescription('Chord progression formulae may appear to be a difficult math equation at first glance. However, they're not as difficult as you may believe. Roman numerals, which represent notes in a scale, are used to show these charts.\r\nIt'll make coming up with root notes a lot easier. Keep in mind that these formulas are tools, not rules. You'll come up with chord progressions that sound really nice and natural if you follow these guidelines.')
-	.addFields(
-		{ name: 'Major keys', value: 'fig. 1.1', inline: true },
-		{ name: 'Minor keys', value: 'fig. 1.2', inline: true },
-	)
-	.setImage('https://cdn.discordapp.com/attachments/889976693102628904/921374821542166528/20211217_174418.jpg')
-	.setFooter('Source - musicnotes.com | \u25CB\u25CF');
-
-let pages = [embed1, embed2];
+let pages = ["test1", "test2"];
 
 simplydjs.embedPages(client, message, pages, {
   firstEmoji: "927629391129903154",
