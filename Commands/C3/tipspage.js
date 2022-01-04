cfd9700d3a96da25bab381e7ca3df87902738ff2
Lethiferal <95504439+lethiferal@users.noreply.github.com>
2022-01-05 02:03:14 +0530

    Update tipspage.js

--- Commands/C3/tipspage.js
+++ Commands/C3/tipspage.js
@@ -20,7 +20,7 @@ let tip2 = new MessageEmbed()
   .setAuthor({ name: 'Production and Workflow Advice | Page - 2', iconURL: 'https://cdn.discordapp.com/attachments/889976693102628904/921376597313978378/20211217_175129.png'})
   .setDescription('Use only a true sine wave as your bass below 100 Hz for maximum clarity and control in your low end. Instead of a muddy low end, you will get a clean, powerful one. Too many conflicting low end bass layers often result in a rumbling low end.')
   .setImage('https://cdn.discordapp.com/attachments/889976693102628904/928006444333301831/InShot_20220105_005559554.jpg')
-  .setFooter('Available pages - ●○')
+  .setFooter('Available pages - ○●')
   .setColor('#2f3135');
   
 let pages = [tip1, tip2];
