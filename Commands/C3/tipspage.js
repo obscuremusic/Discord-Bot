7fc1c59787fac35443415e60dd5560c3b2e260e5
Lethiferal <95504439+lethiferal@users.noreply.github.com>
2022-01-04 18:39:49 +0530

    Update tipspage.js

--- Commands/C3/tipspage.js
+++ Commands/C3/tipspage.js
@@ -8,10 +8,65 @@ code:`$djsEval[(async () => {
 const { MessageEmbed } = require('discord.js');
 const simplydjs = require('simply-djs');
 
-let tip1 = new MessageEmbed().setTitle("Page 1");
+let tip1 = {
+  "description": "Chord progression formulae may appear to be a difficult math equation at first glance. However, they are not as difficult as you may believe. Roman numerals, which represent notes in a scale, are used to show these charts.\r\nIt will make coming up with root notes a lot easier. Keep in mind that these formulas are tools, not rules. You will come up with chord progressions that sound really nice and natural if you follow these guidelines.",
+  "color": 1,
+  "footer": {
+    "text": "Source - musicnotes.com | ●○"
+  },
+  "thumbnail": {
+    "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+  },
+  "image": {
+    "url": "https://cdn.discordapp.com/attachments/889976693102628904/921374821542166528/20211217_174418.jpg"
+  },
+  "author": {
+    "name": "Music Theory Advice",
+    "icon_url": "https://cdn.discordapp.com/attachments/889976693102628904/921376597313978378/20211217_175129.png"
+  },
+  "fields": [
+    {
+      "name": "Minor keys",
+      "value": "Fig 1.1",
+      "inline": true
+    },
+    {
+      "name": "Major keys",
+      "value": "Fig 1.2",
+      "inline": true
+    }
+  ]
+};
 
-let tip2 = new MessageEmbed().setTitle("Page 2");
-
+let tip2 = {
+  "description": "Chord progression formulae may appear to be a difficult math equation at first glance. However, they are not as difficult as you may believe. Roman numerals, which represent notes in a scale, are used to show these charts.\r\nIt will make coming up with root notes a lot easier. Keep in mind that these formulas are tools, not rules. You will come up with chord progressions that sound really nice and natural if you follow these guidelines.",
+  "color": 1,
+  "footer": {
+    "text": "Source - musicnotes.com | ●○"
+  },
+  "thumbnail": {
+    "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+  },
+  "image": {
+    "url": "https://cdn.discordapp.com/attachments/889976693102628904/921374821542166528/20211217_174418.jpg"
+  },
+  "author": {
+    "name": "Music Theory Advice",
+    "icon_url": "https://cdn.discordapp.com/attachments/889976693102628904/921376597313978378/20211217_175129.png"
+  },
+  "fields": [
+    {
+      "name": "Minor keys",
+      "value": "Fig 1.1",
+      "inline": true
+    },
+    {
+      "name": "Major keys",
+      "value": "Fig 1.2",
+      "inline": true
+    }
+  ]
+};
 let pages = [tip1, tip2]
 
 simplydjs.embedPages(client, message, pages, {
