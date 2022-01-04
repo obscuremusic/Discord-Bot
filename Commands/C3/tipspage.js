546b3104112fe9feb95333bf7208912975f54522
Cinerarius <96297750+cinerarius@users.noreply.github.com>
2022-01-04 08:19:43 -0800

    Update tipspage.js

--- Commands/C3/tipspage.js
+++ Commands/C3/tipspage.js
@@ -9,9 +9,11 @@ code:`$djsEval[(async () => {
 const { MessageEmbed } = require('discord.js');
 const simplydjs = require('simply-djs');
 
-let tip1 = new MessageEmbed().setTitle("Paginator page 1");
+let tip1 = new MessageEmbed()
+  .setTitle("Paginator page 1");
 
-let tip2 = new MessageEmbed().setTitle("Paginator page 2");
+let tip2 = new MessageEmbed()
+  .setTitle("Paginator page 2");
 
 let pages = [tip1, tip2];
 
